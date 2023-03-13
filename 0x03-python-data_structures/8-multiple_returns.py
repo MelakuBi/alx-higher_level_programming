7cfe2506b3791132b4890ffe1dac741429746fc6
Melaku Birhanu <melakubirhanu@gmail.com>
2023-03-13 12:52:35 -0700

    $msg

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
+es (7 sloc) 187 Bytes
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
         my_tuple = (0, None)
     else:
         my_tuple = (len(sentence), sentence[:1])
-    return(my_tuple)
+    return(my_tupl)
