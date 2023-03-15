a23d79c3d713995ef45fc19af1d6342f8c1f8697
Melaku Birhanu <melakubirhanu@gmail.com>
2023-03-15 13:13:35 -0700

    $msg

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    return len(sentence), sentence[0] if sentence else None
+def simple_delete(a_dictionary, key=""):
+    a_dictionary.pop(key, None)
+    return a_dictionary
