7909bb1b57c19553d62605c11ae73a0de86b06ec
Melaku Birhanu <melakubirhanu@gmail.com>
2023-03-15 13:16:09 -0700

    $msg

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary.keys():
-        a_dictionary.pop(key)
-    return(a_dictionary)
+def multiply_by_2(a_dictionary):
+    return {k: v * 2 for k, v in a_dictionary.items()})
