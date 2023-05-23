ef35116d8b76249444d727cc5ccd85fc158077b0
MelakuBi <MelakuBirhanu@gmail.com>
2023-05-23 12:15:07 -0700

    city by state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,10 @@
 -- Cities by States
-SELECT c.`id`, s.`name`, states.`name` 
-	FROM cities AS c 
-	INNER JOIN states AS s
-	ON c.id = s.id
-	ORDER BY c.id;
+-- SELECT c.`id`, c.`name`, s.`name` 
+--	FROM cities AS c 
+--	INNER JOIN states AS s
+--	ON c.state_id = s.id
+--	ORDER BY c.id;
+
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
