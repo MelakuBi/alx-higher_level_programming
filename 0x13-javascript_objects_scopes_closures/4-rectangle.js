0173245d23b480e178290086e367b30cb44f6794
MelakuBi <MelakuBirhanu@gmail.com>
2023-06-15 09:16:28 -0700

    $msg

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,11 +4,11 @@ class Rectangle
 {
 	constructor(w, h)
 	{
-	if (w > 0 && h > 0)
-		{
-			this.width = w;
-			this.height = h;
-		}
+		if (w > 0 && h > 0)
+			{
+				this.width = w;
+				this.height = h;
+			}
 	}
 	print()
 	{
