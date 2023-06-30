519f13088d688814145b16bfd63496314ea55d3f
MelakuBi <MelakuBirhanu@gmail.com>
2023-06-30 11:54:49 -0700

    0x10 Networking 0

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# cURL - POST parameters
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pric
