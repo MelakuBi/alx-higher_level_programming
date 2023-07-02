299d96c7b42a4368db8aba804781c551bd7d9354
MelakuBi <melakubirhanu>
2023-07-02 10:25:42 -0700

    alx

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+# Task 0. What's my status? #0
 import urllib.request
-if __name__ == "__main__":
-    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-    print('Body response:$')
-    print('\t - type: {}'.format(type(the_page)))
-    print("\t - content: {}".format(the_page))
-    print("\t - utf8 content: {}".format(the_page.decode("utf-8")))
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode("utf-8")))
